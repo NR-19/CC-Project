66a1791b68bc76dee9b08e32ea2f0994f0906e50
jrafaelr06 <rafaelpiresreis@gmail.com>
2021-11-24 10:53:15 +0000

    Servidor UDP

---
+++ TP2/src/UDP/ServidorMain.java
@@ -0,0 +1,7 @@
+package UDP;
+
+public class ServidorMain {
+    public static void main(String[] args) {
+        new ServidorUDP2().start();
+    }
+}
---
+++ TP2/src/UDP/ServidorUDP2.java
@@ -0,0 +1,46 @@
+package UDP;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class ServidorUDP2 extends Thread {
+    private DatagramSocket socket;
+    private boolean running;
+    private byte[] buf =  new byte[256];
+
+    public ServidorUDP2() {
+        try {
+            this.socket = new DatagramSocket(4445);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        running = true;
+
+        try {
+            while (running) {
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+
+                InetAddress address = packet.getAddress();
+                int port = packet.getPort();
+                packet = new DatagramPacket(buf, buf.length, address, port);
+                String received = new String(packet.getData(), 0, packet.getLength());
+
+                if (received.equals("end")) {
+                    running = false;
+                    continue;
+                }
+                socket.send(packet);
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
