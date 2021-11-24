619899ff2c5065aed5af97241a39481bb579a8b7
jrafaelr06 <rafaelpiresreis@gmail.com>
2021-11-24 10:33:09 +0000

    organizar pastas

--- TP2/src/UDP/ServidorUDP.java
+++ TP2/src/UDP/ServidorUDP.java
@@ -1,8 +1,9 @@
+package UDP;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
 
 public class ServidorUDP {
 
