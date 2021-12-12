d58a85e4194e9418925cef3816dd4f004c6d05ed
jrafaelr06 <rafaelpiresreis@gmail.com>
2021-12-12 19:12:48 +0000

    Alterei o numero da porta e nome do ficheiro

--- TP2/src/FFSync.java
+++ TP2/src/FFSync.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
+import java.net.*;
 
 public class FFSync {
     public static void main(String[] args) {
@@ -13,20 +12,45 @@ public class FFSync {
         System.out.println("Pasta: " + args[0]);
         System.out.println("IP: " + args[1]);
 
+        //Efetuar request a um peer no momento do run da app
         new Thread(() -> {
-            boolean running = true;
-            int port = 80;
+            try {
+                InetAddress ip = InetAddress.getByName(args[1]);
+                byte[] data = { 0x1, 0x2 };
+                int port = 8888;
+
+                DatagramPacket request = new DatagramPacket(data, data.length, ip, port);
+                DatagramSocket socket = new DatagramSocket();
+
+                socket.send(request);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+
+        //Receber pedidos de peers e responder
+        new Thread(() -> {
+            int port = 8888;
+            try {
+                InetAddress host = InetAddress.getLocalHost();
+                String hostName = host.getHostName();
+                System.out.println(hostName);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+
             System.out.println("Listening on port: " + port);
 
             try {
                 DatagramSocket serverSocket = new DatagramSocket(port);
 
-                while(running) {
+                while(true) {
                     byte[] inBuffer = new byte[1500];
                     DatagramPacket inPacket = new DatagramPacket(inBuffer, inBuffer.length);
                     serverSocket.receive(inPacket);
 
-                    // Criar a thread
                     ClientHandler ch = new ClientHandler(inPacket);
                     Thread t = new Thread(ch);
                     t.start();
