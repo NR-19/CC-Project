9528e6055b9d9b743794018758c82027b56ffe70
Mario Santos <mariosantosnn@gmail.com>
2021-12-23 16:53:07 +0000

    minor

--- TP2/src/LogBuilder.java
+++ TP2/src/LogBuilder.java
@@ -27,8 +27,8 @@ public abstract class LogBuilder {
         FileWriter fileWriter = new FileWriter("logs.txt");
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         bufferedWriter.write(text+"\n");
-        fileWriter.close();
-        bufferedWriter.close();
+        fileWriter.flush();
+        bufferedWriter.flush();
     }
 
 }
